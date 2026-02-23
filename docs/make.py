e8cd5683a73ab647bc7cbfc18ca84195b575ac93
shBLOCK <shblock3@gmail.com>
2026-02-23 13:33:02 -0800

    Debug mdBook workflow

--- docs/make.py
+++ docs/make.py
@@ -5,12 +5,12 @@ from pathlib import Path
 DOCS_ROOT = Path(".")
 MISC_ROOT = DOCS_ROOT / "misc"
 
-print("Generating theme/highlight.js")
+print("Generating theme/highlight.js", flush=True)
 with open(DOCS_ROOT / "theme/highlight.js", "w") as highlight_js_file:
     highlight_js_file.write(open(MISC_ROOT / "hljs/highlight.min.js").read())
     highlight_js_file.write("\n" * 3)
     highlight_js_file.write(open(MISC_ROOT / "highlight_addon.js").read())
 
 cmd = ["mdbook", *sys.argv[1:]]
-print(" ".join(cmd))
+print(" ".join(cmd), flush=True)
 exit(subprocess.run(cmd).returncode)
