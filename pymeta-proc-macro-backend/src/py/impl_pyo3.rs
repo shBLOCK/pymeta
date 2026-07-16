fce5c31f23fedfc32ae57abc312bbb1a60103ba7
shblock <shblock3@gmail.com>
2026-07-16 12:54:58 +0800

    Make `linux_force_load_python_lib` only take effect if running as actual proc-macro

--- pymeta-proc-macro-backend/src/py/impl_pyo3.rs
+++ pymeta-proc-macro-backend/src/py/impl_pyo3.rs
@@ -50,7 +50,7 @@ macro_rules! _importer_files_dict_item {
 fn initialize() {
     static INITIALIZED: OnceLock<()> = OnceLock::new();
     INITIALIZED.get_or_init(|| {
-        #[cfg(all(target_os = "linux", feature = "linux_force_load_python_lib"))]
+        #[cfg(all(target_os = "linux", feature = "proc_macro", feature = "linux_force_load_python_lib"))]
         unsafe {
             let libpython = c_str!(concat!("lib", env!("PYMETA_LIBPYTHON_NAME"), ".so"));
             libc::dlopen(libpython.as_ptr(), libc::RTLD_NOW | libc::RTLD_GLOBAL);
