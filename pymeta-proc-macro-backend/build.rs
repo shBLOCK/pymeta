2c3304f680829c4113d5fad7809152e265b7cc00
shblock <shblock3@gmail.com>
2026-07-16 19:18:22 +0800

    Check is Python version is supported in build.rs

--- pymeta-proc-macro-backend/build.rs
+++ pymeta-proc-macro-backend/build.rs
@@ -1,4 +1,24 @@
+use pyo3_build_config::{InterpreterConfig, PythonImplementation, PythonVersion};
+use std::process::exit;
+
+fn check_is_supported(config: &InterpreterConfig) {
+    if config.implementation() != PythonImplementation::CPython {
+        println!("cargo::error=PyMeta: {} is not supported", config.implementation());
+        exit(1);
+    }
+
+    const MIN_PYTHON_VERSION: PythonVersion = PythonVersion { major: 3, minor: 12 };
+    if config.version() < MIN_PYTHON_VERSION {
+        println!(
+            "cargo::error=PyMeta: Python {} is not supported, minimal supported version is {MIN_PYTHON_VERSION}",
+            config.version()
+        );
+        exit(1);
+    }
+}
+
 fn main() {
     let config = pyo3_build_config::get();
+    check_is_supported(config);
     println!("cargo:rustc-env=PYMETA_LIBPYTHON_NAME={}", config.lib_name().unwrap());
 }
